c33f36e3415ef8f29b42c49b6dae6368dee898ed
somanath <somanath039@gmail.com>
2017-06-08 22:51:31 +0530

    odd or even number

---
+++ guvi/OddOrEven.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class OddOrEven {
+	public startic void main(String arg[]) {
+			Scanner Scan = new Scanner (System.in);
+			int inum=  scan.nextInt();
+			if(inum%2 == 0){
+				System.out.println("odd");
+			}else{
+				System.out.println("even");			
+			}
+	}
+}
