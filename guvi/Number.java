411ddbad746edb7c7ca2f9bc0dbd247a763d4733
somanath <somanath039@gmail.com>
2017-06-08 22:41:39 +0530

    guvi_Number

---
+++ guvi/Number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Number {
+	public static void main (String[] args) {
+	    Scanner scan=new Scanner(System.in);
+	    int iNumber=scan.nextInt();
+	    if(iNumber > 0){
+	        System.out.println("Positive");
+	    }
+	    if(iNumber < 0){
+	        System.out.println("Negative");
+	    }
+	    if(iNumber == 0){
+	        System.out.println("Zero");
+	    }
+	    
+	}
+}
